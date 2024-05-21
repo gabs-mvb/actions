bf34d48f08f5c6c7bcec91869c935401a06a1a5c
gabs-mvb <gabriel.boas@sptech.school>
2024-05-20 21:26:08 -0300

    initial commit

---
+++ src/main/java/sptech/actions/controller/CalculadoraController.java
@@ -0,0 +1,23 @@
+package sptech.actions.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sptech.actions.service.CalculadoraService;
+
+@RestController
+public class CalculadoraController {
+
+    private final CalculadoraService calculadoraService;
+
+    public CalculadoraController(CalculadoraService calculadoraService) {
+        this.calculadoraService = calculadoraService;
+    }
+
+    @PostMapping("/divisao/{a}/{b}")
+    public ResponseEntity<Double> divisao(@PathVariable double a, @PathVariable double b) {
+        return ResponseEntity.ok(calculadoraService.dividir(a, b));
+    }
+}
---
+++ src/main/java/sptech/actions/service/CalculadoraService.java
@@ -0,0 +1,15 @@
+package sptech.actions.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class CalculadoraService {
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não é possível dividir por zero");
+        }
+        return a / b;
+    }
+}
